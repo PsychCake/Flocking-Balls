8d1ffbb483f3cddee33825e2a7b0db086458cfe2
Seewald Maximilian <Maximilian-Seewald@gmx.net>
2018-04-25 17:19:34 +0200

    Add files via upload

---
+++ Movement.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    public GameObject[] spheres;
+    public GameObject cube;
+	
+	// Update is called once per frame
+	void Update () {
+        mov();
+    }
+
+   private void mov()
+    {
+        for (int i = 0; i < spheres.Length; i++)
+        {
+            for (int j = 0; j < spheres.Length; j++)
+            {
+                if (Vector3.Distance(spheres[i].transform.position, spheres[j].transform.position) < 1f)
+                {
+                    
+                }else{
+                    spheres[i].transform.position = Vector3.MoveTowards(spheres[i].transform.position, cube.transform.position, (float).1);
+                }
+            }
+        }
+    }
+}
+
---
+++ Spawner.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+    public GameObject cube;
+    public GameObject guide;
+
+	// Use this for initialization
+	void Start () {
+        updateCube();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        moveGuide();
+        if(Vector3.Distance(cube.transform.position,guide.transform.position) < .1f)
+        {
+            updateCube();
+        }
+        //updateCube();
+	}
+
+    private void moveGuide()
+    {
+        guide.transform.position = Vector3.MoveTowards(guide.transform.position, cube.transform.position, .1f);
+    }
+
+    private void updateCube()
+    {
+        Vector3 randomPos = new Vector3(Random.Range(-10f, 10f), Random.Range(0.5f, 10f), Random.Range(-10f, 10f));
+        Quaternion rotation = new Quaternion(0f, 0f, 0f, 0f);
+        cube.transform.SetPositionAndRotation(randomPos, rotation);
+    }
+}
